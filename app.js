7e91316b4450f89352eb3769ebe5384cb856543a
Debabrata Das <debabrata.dd@gmail.com>
2020-10-19 10:12:46 +0530

    Project files added

---
+++ app.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+process.env.MONGO_URI =
+  "mongodb+srv://user01:Password@123@cluster0.7dwye.mongodb.net/Project01?retryWrites=true&w=majority";
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//--> 7)  Mount the Logger middleware here
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+// --> 11)  Mount the body-parser middleware  here
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+/** 1) Meet the node console. */
+//console.log("Hello World");
+
+/** 2) A first working Express Server */
+//app.listen(process.env.PORT, () => console.log("Server Ready..."));
+
+/** 3) Serve an HTML file */
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname + "/public"));
+
+/** 5) serve JSON on a specific route */
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase")
+    res.json({ message: "HELLO JSON" });
+  res.json({ message: "Hello json" });
+});
+
+/** 6) Use the .env file to configure the app */
+process.env.MESSAGE_STYLE = "uppercase";
+
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+/** 8) Chaining middleware. A Time server */
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
+/** 9)  Get input from client - Route parameters */
+app.get("/:word/echo", (req, res) => {
+  res.send({ echo: req.params.word });
+});
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+app.get("/name", (req, res) => {
+  res.send({ name: req.query.first + " " + req.query.last });
+});
+
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+app.post("/name", (req, res) => {
+  res.send({ name: req.body.first + " " + req.body.last });
+});
+
+app.listen(3000, () => console.log("Server ready"));
